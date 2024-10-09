dc30a423f2b3b9308c44d48ccd275e2f864e792d
dailyspecial_719 <dailyspecial_719@yahoo.com>
2024-10-09 16:09:14 -0600

    added ChatroomModel and Firebase functionality

--- app/src/main/java/com/example/gossup/ChatActivity.java
+++ app/src/main/java/com/example/gossup/ChatActivity.java
@@ -13,20 +13,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.gossup.model.ChatroomModel;
 import com.example.gossup.model.UserModel;
 import com.example.gossup.utils.AndroidUtil;
+import com.example.gossup.utils.FirebaseUtil;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.Arrays;
 
 public class ChatActivity extends AppCompatActivity {
 
     UserModel otherUser;
+    String chatroomId;
+    ChatroomModel chatroomModel;
+
 
     TextView otherUsername;
     EditText chatMessageInput;
     ImageButton sendMessage;
     ImageButton backBtn;
     RecyclerView recyclerView;
-
-
 
 
     @Override
@@ -43,6 +51,8 @@ public class ChatActivity extends AppCompatActivity {
         //get UserModel
 
         otherUser = AndroidUtil.getUserModelFromIntent(getIntent());
+        chatroomId = FirebaseUtil.getChatroomId(FirebaseUtil.currentUserId(), otherUser.getUserId());
+
         chatMessageInput = findViewById(R.id.chat_message_input);
         otherUsername = findViewById(R.id.other_username);
         sendMessage = findViewById(R.id.message_send_btn);
@@ -53,12 +63,36 @@ public class ChatActivity extends AppCompatActivity {
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 getOnBackPressedDispatcher().onBackPressed();
             }
         });
 
         otherUsername.setText(otherUser.getUsername());
 
+        getOrCreateChatroomModel();
+
 
     }
+
+    void getOrCreateChatroomModel() {
+        FirebaseUtil.getChatroomReference(chatroomId).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                chatroomModel = task.getResult().toObject(ChatroomModel.class);
+                if (chatroomModel == null) {
+                    //first time chat
+                    chatroomModel = new ChatroomModel(
+                            chatroomId, Arrays.asList(FirebaseUtil.currentUserId(), otherUser.getUserId()),
+                            Timestamp.now(),
+                            ""
+
+                    );
+                    FirebaseUtil.getChatroomReference(chatroomId).set(chatroomModel);
+
+
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/gossup/model/ChatroomModel.java
+++ app/src/main/java/com/example/gossup/model/ChatroomModel.java
@@ -1,4 +1,57 @@
 package com.example.gossup.model;
 
+import java.security.Timestamp;
+import java.util.List;
+
 public class ChatroomModel {
+
+    String chatroomId;
+    List<String> userIds;
+    Timestamp lastMessageTimeStamp;
+    String laseMessageSenderId;
+
+    public ChatroomModel() {
+    }
+
+    public ChatroomModel(String chatroomId, List<String> userIds, Timestamp lastMessageTimeStamp, String laseMessageSenderId) {
+        this.chatroomId = chatroomId;
+        this.userIds = userIds;
+        this.lastMessageTimeStamp = lastMessageTimeStamp;
+        this.laseMessageSenderId = laseMessageSenderId;
+    }
+
+    public ChatroomModel(String chatroomId, List<String> list, com.google.firebase.Timestamp now, String laseMessageSenderId) {
+    }
+
+    public String getChatroomId() {
+        return chatroomId;
+    }
+
+    public void setChatroomId(String chatroomId) {
+        this.chatroomId = chatroomId;
+    }
+
+    public List<String> getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(List<String> userIds) {
+        this.userIds = userIds;
+    }
+
+    public Timestamp getLastMessageTimeStamp() {
+        return lastMessageTimeStamp;
+    }
+
+    public void setLastMessageTimeStamp(Timestamp lastMessageTimeStamp) {
+        this.lastMessageTimeStamp = lastMessageTimeStamp;
+    }
+
+    public String getLaseMessageSenderId() {
+        return laseMessageSenderId;
+    }
+
+    public void setLaseMessageSenderId(String laseMessageSenderId) {
+        this.laseMessageSenderId = laseMessageSenderId;
+    }
 }
